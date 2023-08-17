3a70222b3cc860a8063d5e0d970948ad49534617
Nicholas Hart <nickhart@gmail.com>
2023-08-17 14:43:39 -0700

    implemented sudoku number placement rules! made the game size dynamic (eg: degree: 2 => 4x4 grid) updated launch json

--- pages/tictactoe.tsx
+++ pages/tictactoe.tsx
@@ -64,12 +64,58 @@ interface GameProps {
   currentNumber?: number;
 }
 
+function rowAndColumnFromIndex(index: number, degree: number) {
+  const size = degree*degree;
+  let col = index % size;
+  let row = (index - col) / size;
+  return { row:row, col:col };
+}
+
 function numberForStep(step: number): number {
   return 0;
 }
 
-function isLegalPlay(cells: Array<number>, i: number, choice: number): boolean {
-  return choice ? !cells[i] && !cells.includes(choice) : true;
+function isLegalPlay(cells: Array<number>, i: number, choice: number, degree: number): boolean {
+  if (!choice) {
+    // can always clear?
+    return true;
+  }
+  if (cells[i]) {
+    // can never overwrite
+    return false;
+  }
+
+  const { row, col } = rowAndColumnFromIndex(i, degree);
+  const rowValues = cellValuesForRow(row, degree);
+  const rowNumbers = rowValues.map((value) => cells[value]);
+  if (rowNumbers.includes(choice)) {
+    console.log(`number ${choice} is already in row ${row} (${rowNumbers})`);
+    return false;
+  }
+  const colValues = cellValuesForColumn(col, degree);
+  const colNumbers = colValues.map((value) => cells[value]);
+  if (colNumbers.includes(choice)) {
+    console.log(`number ${choice} is already in column ${col} (${colNumbers})`);
+    return false;
+  }
+
+  // eg: for degree 2...
+  // [0, 1, 2, 3]
+  // [4, 5, 6, 7]
+  // [8, 9, 10, 11]
+  // [12, 13, 14, 15]
+  // square 0,0 x 1,1 = [0, 1, 4, 5]
+  // square 0,2 x 1,3 = [2, 3, 6, 7]
+  // square 2,0 x 3,1 = [8, 9, 12, 13]
+  // square 2,2 x 3,3 = [10, 11, 14, 15] 
+  const squareValues = cellValuesForSquare(row, col, degree);
+  const squareNumbers = squareValues.map((value) => cells[value]);
+  if (squareNumbers.includes(choice)) {
+    console.log(`number ${choice} is already in sqaure containing cell ${row},${col} (${squareNumbers})`);
+    return false;
+  }
+
+  return true;
 }
 
 function isGameOver(cells: Array<number>): boolean {
@@ -80,10 +126,10 @@ function newGameForDegree(degree: number): number[] {
   return Array(degree*degree*degree*degree).fill(0);
 }
 
-function generateAscendingArray(start: number, length: number): number[] {
+function generateAscendingArray(start: number, length: number, multiple: number = 1): number[] {
   const result: number[] = [];
   for (let i = 0; i < length; i++) {
-    result.push(start + i);
+    result.push(start + i * multiple);
   }
   return result;
 }
@@ -97,6 +143,7 @@ function choicesForDegree(degree: number): Array<number> {
 //   return generateAscendingArray(start, degree*degree);
 // }
 
+// todo: refactor this
 function rowValuesForDegree(degree: number) {
   let result = [];
   const size = degree*degree;
@@ -104,6 +151,33 @@ function rowValuesForDegree(degree: number) {
     result.push(generateAscendingArray(i * size, size));
   }
   console.log(`rowValues: ${result}`);
+  return result;
+}
+
+function cellValuesForRow(row: number, degree: number) {
+  const size = degree*degree;
+  return generateAscendingArray(size * row, size);
+}
+
+function cellValuesForColumn(column: number, degree: number) {
+  const size = degree*degree;
+  return generateAscendingArray(column, size, size);
+}
+
+function cellValuesForSquare(row: number, col: number, degree: number) {
+  const size = degree*degree;
+  let result = [];
+
+  // convert the row and column to the starting row and column for whichever square it lies within
+  const rowStart = row - row%degree;
+  const colStart = col - col%degree;
+
+  // instead of a row or column we want to concat N rows, where N is the degree
+  for (let index = 0; index != degree; ++index) {
+    const start = (rowStart + index) * size + colStart;
+    const array = generateAscendingArray(start, degree);
+    result = result.concat(array);
+  }
   return result;
 }
 
@@ -133,6 +207,7 @@ class Game extends React.Component<GameProps, any> {
     const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const cells = current.cells.slice();
+    const degree = this.state.degree;
     if (isGameOver(cells)) {
       console.log("ignoring: game is over");
       return;
@@ -142,7 +217,7 @@ class Game extends React.Component<GameProps, any> {
       console.log("ignoring: input is unchanged");
       return;
     }
-    if (!isLegalPlay(cells, i, currentNumber)) {
+    if (!isLegalPlay(cells, i, currentNumber, degree)) {
       console.log("ignoring: not a legal play");
       return;
     }
