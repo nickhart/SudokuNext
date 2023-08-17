38518e272f5f59e94b8b5f7a5f86ba0fc432fd10
Nicholas Hart <nickhart@gmail.com>
2023-08-16 21:48:35 -0700

    implemented number chooser

--- pages/tictactoe.tsx
+++ pages/tictactoe.tsx
@@ -1,10 +1,10 @@
-import React, { MouseEvent } from 'react';
+import React, { MouseEvent } from "react";
 // import { assert } from 'console'
 
 function Cell(props: any) {
   return (
     <button className="cell" onClick={props.onClick}>
-      { props.value }
+      {props.value}
     </button>
   );
 }
@@ -13,49 +13,62 @@ type GameState = Array<number>;
 
 interface BoardProps {
   cells: GameState;
-  onClick: (index: number) => void;
+  onCellClick: (index: number) => void;
+  onChoiceClick: (index: number) => void;
 }
 
 class Board extends React.Component<BoardProps, any> {
   renderCell(i: number) {
     return (
       <Cell
-        value={this.props.cells[i]} 
-        key={i}
-        onClick={() => this.props.onClick(i)}
+        value={this.props.cells[i]}
+        key={`cell${i}`}
+        onClick={() => this.props.onCellClick(i)}
+      />
+    );
+  }
+
+  renderChoice(i: number) {
+    return (
+      <Cell
+        value={i}
+        key={`choice${i}`}
+        onClick={() => this.props.onChoiceClick(i)}
       />
     );
   }
 
   render() {
-    const rowValues = [[0, 1, 2], [3, 4, 5], [6, 7, 8]];
+    const choiceValues = [0, 1, 2, 3];
+    const rowValues = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+    ];
     return (
       <div>
         <div className="board-row">
-          { 
-            rowValues[0].map((value: number) => this.renderCell(value))
-          }
+          {choiceValues.map((value: number) => this.renderChoice(value))}
+        </div>
+        <br />
+        <div className="board-row">
+          {rowValues[0].map((value: number) => this.renderCell(value))}
         </div>
         <div className="board-row">
-          { 
-            rowValues[1].map((value: number) => this.renderCell(value))
-          }
+          {rowValues[1].map((value: number) => this.renderCell(value))}
         </div>
         <div className="board-row">
-          { 
-            rowValues[2].map((value: number) => this.renderCell(value))
-          }
+          {rowValues[2].map((value: number) => this.renderCell(value))}
         </div>
       </div>
     );
   }
 }
-  
+
 interface GameProps {
   history?: GameState[];
   stepNumber?: number;
   currentNumber?: number;
-
 }
 
 function numberForStep(step: number): number {
@@ -63,22 +76,22 @@ function numberForStep(step: number): number {
 }
 
 function calculateWinner(cells: Array<number>) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (cells[a] && cells[a] === cells[b] && cells[a] === cells[c]) {
-      return cells[a];
-    }
-  }
+  // const lines = [
+  //   [0, 1, 2],
+  //   [3, 4, 5],
+  //   [6, 7, 8],
+  //   [0, 3, 6],
+  //   [1, 4, 7],
+  //   [2, 5, 8],
+  //   [0, 4, 8],
+  //   [2, 4, 6],
+  // ];
+  // for (let i = 0; i < lines.length; i++) {
+  //   const [a, b, c] = lines[i];
+  //   if (cells[a] && cells[a] === cells[b] && cells[a] === cells[c]) {
+  //     return cells[a];
+  //   }
+  // }
   return null;
 }
 
@@ -88,69 +101,78 @@ function newGame(): number[] {
 
 class Game extends React.Component<GameProps, any> {
   constructor(props: any) {
-      super(props);
-      this.state = {
-        history: [{
+    super(props);
+    this.state = {
+      history: [
+        {
           cells: newGame(),
-        }],
-        stepNumber: 0,
-        currentNumber: 1,
-      };
+        },
+      ],
+      stepNumber: 0,
+      currentNumber: 0,
+    };
+  }
+
+  handleChoiceClick(i: number) {
+    this.setState({
+      currentNumber: i,
+    });
+  }
+
+  handleCellClick(i: number) {
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
+    const current = history[history.length - 1];
+    const cells = current.cells.slice();
+    if (calculateWinner(cells) || cells[i]) {
+      return;
     }
+    cells[i] = this.state.currentNumber;
+    this.setState({
+      history: history.concat([
+        {
+          cells: cells,
+        },
+      ]),
+      stepNumber: history.length,
+      currentNumber: this.state.currentNumber,
+    });
+  }
 
-    handleClick(i: number) {
-      const history = this.state.history.slice(0, this.state.stepNumber + 1);
-      const current = history[history.length - 1];
-      const cells = current.cells.slice();
-          if (calculateWinner(cells) || cells[i]) {
-        return;
-      }
-      cells[i] = this.state.currentNumber;
-      this.setState({
-          history: history.concat([{
-              cells: cells,
-            }]),
-            stepNumber: history.length,
-            currentNumber: this.state.currentNumber,
-      });
-    }
+  jumpTo(step: number) {
+    this.setState({
+      stepNumber: step,
+      currentNumber: numberForStep(step),
+    });
+  }
 
-    jumpTo(step: number) {
-      this.setState({
-        stepNumber: step,
-        currentNumber: numberForStep(step)
-      });
-    }
-  
-    render() {
-      const history = this.state.history;
-      const current = history[this.state.stepNumber];
-      const winner = calculateWinner(current.cells);
+  render() {
+    const history = this.state.history;
+    const current = history[this.state.stepNumber];
+    const winner = calculateWinner(current.cells);
 
-      const moves = history.map((step: any, move: React.Key) => {
-          const desc = move ?
-            'Go to move #' + move :
-            'Go to game start';
-          return (
-              <li key={move}>
-              <button onClick={() => this.jumpTo(Number(move))}>{desc}</button>
-            </li>
-          );
-        });
-    
-      let status;
-      if (winner) {
-        status = 'Winner!';
-      } else {
-        status = 'current number: ' + this.state.currentNumber;
-      }
-
+    const moves = history.map((step: any, move: React.Key) => {
+      const desc = move ? "Go to move #" + move : "Go to game start";
       return (
+        <li key={move}>
+          <button onClick={() => this.jumpTo(Number(move))}>{desc}</button>
+        </li>
+      );
+    });
+
+    let status;
+    if (winner) {
+      status = "Winner!";
+    } else {
+      status = "current number: " + this.state.currentNumber;
+    }
+
+    return (
       <div className="game">
         <div className="game-board">
           <Board
-              cells={current.cells}
-              onClick={(i) => this.handleClick(i)}
+            cells={current.cells}
+            onCellClick={(i) => this.handleCellClick(i)}
+            onChoiceClick={(i) => this.handleChoiceClick(i)}
           />
         </div>
         <div className="game-info">
@@ -162,11 +184,10 @@ class Game extends React.Component<GameProps, any> {
   }
 }
 
-
 // ========================================
 
 function App() {
-  return <Game />
+  return <Game />;
 }
 
-export default App
+export default App;
